b91f60b51d1422209b1b78cbb612f151b375f7be
Hamjy09 <hamjyjoker970@gmail.com>
2025-08-24 00:13:43 +0500

    blue arch store frontend started

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from "./components/navbar" 
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <h1 className='text-red-400'>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
