300aba13eaabeb9f8d88ca879ffd8844d8d42448
Hamjy09 <hamjyjoker970@gmail.com>
2025-08-25 04:06:30 +0500

    changes in sidebar done

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 
-const Sidebar = ({ isOpen, onClose }) => {
-  if (!isOpen) return null;
+const Sidebar = (props) => {
+  if (!props.isOpen) return null;
 
   return (
-    <div className="fixed inset-0 z-50 flex">
-      {/* Overlay */}
-      <div
-        className="fixed inset-0 bg-black opacity-50"
-        onClick={onClose}
-      ></div>
+    <div className={`fixed top-0 right-0 h-full w-64 bg-white shadow-lg transform transition-transform duration-2000 ease-in-out ${
+    props.isOpen ? "translate-x-0" : "translate-x-full"
+  }`}
+  >
+      
 
       {/* Sidebar content */}
       <div className="relative bg-white w-64 h-full shadow-lg p-4">
         <button
-          className="mb-4 text-blue-500"
-          onClick={onClose}
+          className="text-3xl text-blue-500 absolute top-1 right-4"
+          onClick={props.onClose}
         >
-          Close
+          x
         </button>
-        <ul className="flex flex-col gap-4">
+        <div className="pt-40">
+            <ul className="flex flex-col gap-7 items-center font-jakarta text-l font-bold leading-[30px] text-blue-500">
           <li>Home</li>
           <li>About</li>
           <li>Services</li>
           <li>Contact</li>
         </ul>
+        </div>
+        
       </div>
     </div>
   );
